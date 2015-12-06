ba704165c16457af9f6926595cf25187dceaeae0
PellyTsui <pellyxu@qq.com>
2015-12-06 22:39:38 +0800

    Updated
    added 2 tables, cat and dog. Pls modify table "pet" and change the PetId
    type from "varchar" to "int" and auto-increment. Many thanks!

--- pet_health_care.sql
+++ pet_health_care.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50519
 File Encoding         : 65001
 
-Date: 2015-11-06 23:28:59
+Date: 2015-12-06 22:35:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -60,6 +60,63 @@ CREATE TABLE `appointment` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `cat`
+-- ----------------------------
+DROP TABLE IF EXISTS `cat`;
+CREATE TABLE `cat` (
+  `PetKindId` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `PetKindName` varchar(255) NOT NULL,
+  PRIMARY KEY (`PetKindId`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cat
+-- ----------------------------
+INSERT INTO `cat` VALUES ('1', '阿比西尼亚猫');
+INSERT INTO `cat` VALUES ('2', '埃及猫 ');
+INSERT INTO `cat` VALUES ('3', '奥西猫');
+INSERT INTO `cat` VALUES ('4', '巴厘猫');
+INSERT INTO `cat` VALUES ('5', '波米拉猫');
+INSERT INTO `cat` VALUES ('6', '波斯猫');
+INSERT INTO `cat` VALUES ('7', '伯曼猫');
+INSERT INTO `cat` VALUES ('8', '布偶猫');
+INSERT INTO `cat` VALUES ('9', '德文卷毛猫 ');
+INSERT INTO `cat` VALUES ('10', '东方猫');
+INSERT INTO `cat` VALUES ('11', '东奇尼猫');
+INSERT INTO `cat` VALUES ('12', '俄罗斯蓝猫');
+INSERT INTO `cat` VALUES ('13', '哈瓦那棕猫');
+INSERT INTO `cat` VALUES ('14', '呵叻猫');
+INSERT INTO `cat` VALUES ('15', '柯尼斯卷毛猫');
+INSERT INTO `cat` VALUES ('16', '拉邦猫');
+INSERT INTO `cat` VALUES ('17', '褴褛猫');
+INSERT INTO `cat` VALUES ('18', '马恩岛猫');
+INSERT INTO `cat` VALUES ('19', '美国短毛猫');
+INSERT INTO `cat` VALUES ('20', '美国短尾猫');
+INSERT INTO `cat` VALUES ('21', '美国刚毛猫');
+INSERT INTO `cat` VALUES ('22', '美国卷毛猫');
+INSERT INTO `cat` VALUES ('23', '孟买猫');
+INSERT INTO `cat` VALUES ('24', '缅甸猫');
+INSERT INTO `cat` VALUES ('25', '缅因猫');
+INSERT INTO `cat` VALUES ('26', '挪威森林猫');
+INSERT INTO `cat` VALUES ('27', '欧洲缅甸猫');
+INSERT INTO `cat` VALUES ('28', '日本短尾猫');
+INSERT INTO `cat` VALUES ('29', '塞尔凯克卷毛猫');
+INSERT INTO `cat` VALUES ('30', '沙特尔猫');
+INSERT INTO `cat` VALUES ('31', '斯芬克斯猫');
+INSERT INTO `cat` VALUES ('32', '苏格兰折耳猫');
+INSERT INTO `cat` VALUES ('33', '索马里猫');
+INSERT INTO `cat` VALUES ('34', '土耳其安哥拉猫');
+INSERT INTO `cat` VALUES ('35', '土耳其梵猫');
+INSERT INTO `cat` VALUES ('36', '西伯利亚森林猫');
+INSERT INTO `cat` VALUES ('37', '暹罗猫');
+INSERT INTO `cat` VALUES ('38', '新加坡猫');
+INSERT INTO `cat` VALUES ('39', '异国短毛猫');
+INSERT INTO `cat` VALUES ('40', '英国短毛猫');
+INSERT INTO `cat` VALUES ('41', '中国狸花猫');
+INSERT INTO `cat` VALUES ('42', '中华田园猫');
+INSERT INTO `cat` VALUES ('43', '重点色短毛猫');
+
+-- ----------------------------
 -- Table structure for `dailyhealth`
 -- ----------------------------
 DROP TABLE IF EXISTS `dailyhealth`;
@@ -98,6 +155,63 @@ CREATE TABLE `doctor` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `dog`
+-- ----------------------------
+DROP TABLE IF EXISTS `dog`;
+CREATE TABLE `dog` (
+  `PetKindId` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `PetKindName` varchar(255) NOT NULL,
+  PRIMARY KEY (`PetKindId`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dog
+-- ----------------------------
+INSERT INTO `dog` VALUES ('1', '阿富汗猎犬');
+INSERT INTO `dog` VALUES ('2', '阿拉斯加雪橇犬');
+INSERT INTO `dog` VALUES ('3', '爱尔兰猎狼犬');
+INSERT INTO `dog` VALUES ('4', '巴哥犬');
+INSERT INTO `dog` VALUES ('5', '北京犬');
+INSERT INTO `dog` VALUES ('6', '比利时牧羊犬');
+INSERT INTO `dog` VALUES ('7', '边境牧羊犬');
+INSERT INTO `dog` VALUES ('8', '波利犬');
+INSERT INTO `dog` VALUES ('9', '博美犬');
+INSERT INTO `dog` VALUES ('10', '藏獒');
+INSERT INTO `dog` VALUES ('11', '柴犬');
+INSERT INTO `dog` VALUES ('12', '柴犬');
+INSERT INTO `dog` VALUES ('13', '大丹犬');
+INSERT INTO `dog` VALUES ('14', '大麦町犬');
+INSERT INTO `dog` VALUES ('15', '德国牧羊犬');
+INSERT INTO `dog` VALUES ('16', '斗牛梗');
+INSERT INTO `dog` VALUES ('17', '杜宾犬');
+INSERT INTO `dog` VALUES ('18', '法国斗牛犬');
+INSERT INTO `dog` VALUES ('19', '贵宾犬');
+INSERT INTO `dog` VALUES ('20', '哈士奇');
+INSERT INTO `dog` VALUES ('21', '蝴蝶犬');
+INSERT INTO `dog` VALUES ('22', '惠比特犬');
+INSERT INTO `dog` VALUES ('23', '吉娃娃');
+INSERT INTO `dog` VALUES ('24', '金毛寻回犬');
+INSERT INTO `dog` VALUES ('25', '金毛寻回犬');
+INSERT INTO `dog` VALUES ('26', '卷毛比雄犬');
+INSERT INTO `dog` VALUES ('27', '卡斯罗');
+INSERT INTO `dog` VALUES ('28', '柯利犬');
+INSERT INTO `dog` VALUES ('29', '拉布拉多寻回犬');
+INSERT INTO `dog` VALUES ('30', '兰波格犬');
+INSERT INTO `dog` VALUES ('31', '猎兔犬');
+INSERT INTO `dog` VALUES ('32', '美国爱斯基摩犬');
+INSERT INTO `dog` VALUES ('33', '迷你杜宾');
+INSERT INTO `dog` VALUES ('34', '纽芬兰犬');
+INSERT INTO `dog` VALUES ('35', '秋田犬');
+INSERT INTO `dog` VALUES ('36', '萨摩耶犬');
+INSERT INTO `dog` VALUES ('37', '圣伯纳犬');
+INSERT INTO `dog` VALUES ('38', '松狮');
+INSERT INTO `dog` VALUES ('39', '苏俄猎狼犬');
+INSERT INTO `dog` VALUES ('40', '西施犬');
+INSERT INTO `dog` VALUES ('41', '寻血猎犬');
+INSERT INTO `dog` VALUES ('42', '英国斗牛犬');
+INSERT INTO `dog` VALUES ('43', '中国沙皮犬 \r\n中国沙皮犬 \r\n中国沙皮犬 \r\n中国沙皮犬 \r\n中国沙皮犬');
+
+-- ----------------------------
 -- Table structure for `pet`
 -- ----------------------------
 DROP TABLE IF EXISTS `pet`;
@@ -105,9 +219,10 @@ CREATE TABLE `pet` (
   `PetID` varchar(255) NOT NULL,
   `PetName` varchar(255) NOT NULL,
   `Ownername` varchar(255) NOT NULL,
-  `PetSex` varchar(255) NOT NULL,
+  `PetSex` tinyint(1) NOT NULL,
   `PetAge` double(80,0) NOT NULL,
   `PetType` varchar(255) NOT NULL,
+  `PetKind` varchar(255) NOT NULL,
   PRIMARY KEY (`PetID`),
   KEY `Owner_Pet` (`Ownername`),
   CONSTRAINT `Owner_Pet` FOREIGN KEY (`Ownername`) REFERENCES `user` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -215,4 +330,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('a', '111111', 'ari-sa@163.com', '11111111111', '1', '1', '86156907c7fac5b12852978a6959cb28');
+INSERT INTO `user` VALUES ('a', '222222', 'ari-sa@163.com', '11111111111', '1', '1', '86156907c7fac5b12852978a6959cb28');
+INSERT INTO `user` VALUES ('ä½ ', '111111', '1@333.com', '11111111111', '2', '0', null);
